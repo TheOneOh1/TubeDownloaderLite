bde9d07d331d1337e81dc47720fa8cc2ec9b23e9
Anand Kore <103954920+TheOneOh1@users.noreply.github.com>
2022-08-01 23:18:53 +0530

    Update downloadMe.py

--- downloadMe.py
+++ downloadMe.py
@@ -4,20 +4,20 @@ from sys import argv
 link = input("Enter Video Link : " )
 
 try:
-vid = YouTube(link)
+    vid = YouTube(link)
 
-print("\n============================================================================\n")
+    print("\n============================================================================\n")
 
-print("Title : ", vid.title)
+    print("Title : ", vid.title)
 
-down = vid.streams.get_by_itag(22)
-down.download()
+    down = vid.streams.get_by_itag(22)
+    down.download()
 
-#You can save the downloaded video in a specific path down.download('path')
+    #You can save the downloaded video in a specific path down.download('path')
 
-print("\nVideo Download Complete")
+    print("\nVideo Download Complete")
 
-print("\n============================================================================\n")
+    print("\n============================================================================\n")
 
 except Exception as e:
     print("Something went wrong with the link, more information is provided below.")
